3d288752e9eeaabcb17bddf94211bf9b2bdf2252
Piotr Murach <pmurach@gmail.com>
2011-09-24 19:31:29 +0100

    Add rails app to specs and update required gems.

--- Gemfile
+++ Gemfile
@@ -6,7 +6,9 @@ source "http://rubygems.org"
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
 group :development do
+  gem "rails", "3.1.0"
   gem "rspec", "~> 2.4.0"
+  gem "capybara", ">= 0.4.0"
   gem "bundler", "~> 1.0.0"
   gem "jeweler", "~> 1.6.4"
   gem "rcov", ">= 0"
---
+++ spec/rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = '2bc2743124978fc50d054e09cc457b9a6de3cb8c1b7000d78a9af674e93245bf292f2f57f56ad782dc6f7da1695206251d7c9541ec3870467275e4c65447db48'
