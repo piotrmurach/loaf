c6fb3276c4e6a2e8f31614880753bc0691a90509
Piotr Murach <pmurach@gmail.com>
2011-09-24 18:49:07 +0100

    Add todo list.

--- lib/loaf/helpers.rb
+++ lib/loaf/helpers.rb
@@ -6,7 +6,7 @@ module Loaf
 
       @crumbs.each do |crumb|
         
-        name = crumb.name ? truncate(crumb.name.capitalize, :length => 30) : ''
+        name = crumb.name ? truncate(crumb.name.upcase, :length => 30) : ''
         url  = eval(crumb.url)
         styles = ( request.request_uri.split('?')[0] == url ? 'selected' : '' )
        
