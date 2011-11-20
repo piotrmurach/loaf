eb705671a6205d275e80b35ffc38ae29dc7b4759
Piotr Murach <pmurach@gmail.com>
2011-11-20 19:12:35 +0000

    Update gem version.

--- lib/loaf/version.rb
+++ lib/loaf/version.rb
@@ -2,9 +2,9 @@ module Loaf
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 0
+    PATCH = 1
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.');
   end
-end
+end # Loaf
