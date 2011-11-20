b39b166d5d61b348cc9954b5f814935644a9f50d
Piotr Murach <pmurach@gmail.com>
2011-11-20 16:08:26 +0000

    Replace eval function.

--- lib/loaf/helpers.rb
+++ lib/loaf/helpers.rb
@@ -2,7 +2,7 @@ require 'loaf/configuration'
 
 module Loaf
   module Helpers
-    
+
     class_eval do
       define_method :config do |options|
         Loaf.config
@@ -13,17 +13,17 @@ module Loaf
     def add_breadcrumb(name, url=nil)
       _breadcrumbs.push(name, url)
     end
-    
+
     def breadcrumbs(options={}, &block)
       #builder = Loaf::Builder.new(options)
       options = config.merge(options)
 
       _breadcrumbs.each do |crumb|
-        
+
         name = crumb.name ? truncate(crumb.name.upcase, :length => options[:crumb_length]) : ''
-        url  = eval(crumb.url)
+        url  = send(crumb.url)
         styles = ( request.request_uri.split('?')[0] == url ? "#{options[:style_classes]}" : '' )
-       
+
         block.call(name, url, styles)
       end
     end
