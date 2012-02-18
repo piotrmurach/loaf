0a3346865c26705dcb5bb288ab6df8a1a5320e09
Piotr Murach <pmurach@gmail.com>
2012-02-18 19:19:32 +0000

    Update railtie to use renamed classes.

--- lib/loaf/railtie.rb
+++ lib/loaf/railtie.rb
@@ -1,19 +1,28 @@
-module Loaf
+# encoding: utf-8
 
+require 'action_controller'
+require 'action_view'
+
+module Loaf
   if defined? Rails::Railtie
     class Railtie < Rails::Railtie
       initializer "loaf.extend_action_controller_base" do |app|
-        ActiveSupport.on_load(:action_controller) do
-          Loaf::Railtie.insert
+        ActiveSupport.on_load :action_controller do
+          Loaf::Railtie.insert_controller
+          Loaf::Railtie.insert_view
         end
       end
     end
   end
 
   class Railtie
-    def self.insert
-      ActionController::Base.send :include, Loaf::Filters 
-      ActionController::Base.helper Loaf::Helpers
+    class << self
+      def insert_view
+        ActionController::Base.helper Loaf::ViewExtensions
+      end
+      def insert_controller
+        ActionController::Base.send :include, Loaf::ControllerExtensions
+      end
     end
   end # Railtie
 
