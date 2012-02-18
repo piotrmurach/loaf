9ac56817afca0db0ed963edae1ffbe5fc354f49b
Piotr Murach <pmurach@gmail.com>
2012-02-18 18:19:02 +0000

    Update spec helper to use dummy app and remove unnecessary fat.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,30 +1,15 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+# Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
-#require 'rails'
-#require 'rails/test_help'
-#require 'rails'
-#require 'rspec/rails'
-require 'loaf'
 
-require File.expand_path("../rails_app/config/environment.rb", __FILE__)
+require File.expand_path("../rails_app/config/environment.rb",  __FILE__)
+require 'rspec/rails'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
+Rails.backtrace_cleaner.remove_silencers!
+
+# Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
-# Capybara for integration testing
-require 'capybara'
-require 'capybara/rails'
-require 'capybara/rspec'
-Capybara.default_driver   = :rack_test
-Capybara.default_selector = :css
-
 RSpec.configure do |config|
-  #config.include RSpec::Rails::RequestExampleGroup,   
-  #                :example_group => { :file => /spec\/integration/ }
-  config.include Capybara::DSL
-  config.include Rails.application.routes.url_helpers
-
+  config.use_transactional_fixtures = true
   config.mock_with :rspec
 end
