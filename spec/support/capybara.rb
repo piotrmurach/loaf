750ce92716f59ab91714a44dc689d6573e3aed5d
Piotr Murach <pmurach@gmail.com>
2012-02-18 18:17:44 +0000

    Setup capybara integration testing.

---
+++ spec/support/capybara.rb
@@ -0,0 +1,10 @@
+require 'capybara/rails'
+require 'capybara/dsl'
+
+RSpec.configure do |c|
+  c.include Capybara::DSL, :example_group => {
+    :file_path => /\bspec\/integration\//
+  }
+end
+Capybara.default_driver   = :rack_test
+Capybara.default_selector = :css
