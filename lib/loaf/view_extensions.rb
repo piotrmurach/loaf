b81f34bd8b5fa5180654756e8c8558920f648af3
Piotr Murach <pmurach@gmail.com>
2012-02-18 22:58:01 +0000

    Change rails view extensions to use new formatter.

--- lib/loaf/view_extensions.rb
+++ lib/loaf/view_extensions.rb
@@ -1,35 +1,29 @@
 # encoding: utf-8
 
+require 'loaf/crumb_formatter'
+
 module Loaf
   module ViewExtensions
+    include Loaf::CrumbFormatter
 
-    class_eval do
-      define_method :config do |options|
-        Loaf.config
-      end
-    end
-
-    # Adds breadcrumbs in a view
-    def add_breadcrumb(name, url=nil)
+    # Adds breadcrumbs in a view.
+    #
+    def add_breadcrumb(name, url)
       _breadcrumbs.push(name, url)
     end
 
+    # Renders breadcrumbs inside view.
+    #
     def breadcrumbs(options={}, &block)
       #builder = Loaf::Builder.new(options)
-      options = config.merge(options)
+      options = Loaf.config.merge(options)
 
       _breadcrumbs.each do |crumb|
-
-        name = if crumb.name
-          formatted = options[:capitalize] ? crumb.name.capitalize : crumb.name
-          truncate(formatted, :length => options[:crumb_length])
-        else
-          '[name-error]'
-        end
+        name = format_name crumb, options
 
         url = url_for _process_url_for(crumb.url)
 
-        styles = ( request.request_uri.split('?')[0] == url ? "#{options[:style_classes]}" : '' )
+        styles = current_page?(url) ? "#{options[:style_classes]}" : ''
 
         block.call(name, url, styles)
       end
