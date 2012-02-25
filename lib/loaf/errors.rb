01fe2b5d57da9c38d550da66c652586cee3b30a8
Piotr Murach <pmurach@gmail.com>
2012-02-25 15:10:25 +0000

    Add generic loaf gem errors.

---
+++ lib/loaf/errors.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+module Loaf #:nodoc:
+
+  # Default Loaf error for all custom errors.
+  #
+  class LoafError < StandardError
+    BASE_KEY = "loaf.errors.messages"
+
+    def error_message(key, attributes)
+      translate(key, attributes)
+    end
+
+    def translate(key, options)
+      ::I18n.translate("#{BASE_KEY}.#{key}", { :locale => :en }.merge(options))
+    end
+  end
+
+  # Raised when invalid options are passed to breadcrumbs view renderer.
+  #  InvalidOptions.new :name, :crumber, [:crumb]
+  #
+  class InvalidOptions < LoafError
+    def initialize(invalid, valid)
+      super(
+        error_message("invalid_options",
+          { :invalid => invalid, :valid => valid.join(', ') }
+        )
+      )
+    end
+  end
+
+end # Loaf
