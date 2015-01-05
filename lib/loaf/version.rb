a2d4a63da208d5886c0e6664a16a52304b9a4005
Piotr Murach <pmurach@gmail.com>
2015-01-06 00:07:23 +0100

    Simplify version.

--- lib/loaf/version.rb
+++ lib/loaf/version.rb
@@ -1,12 +1,5 @@
 # encoding: utf-8
 
 module Loaf
-  module Version
-    MAJOR = 0
-    MINOR = 3
-    PATCH = 0
-    BUILD = nil
-
-    STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.');
-  end
+  VERSION = '0.3.0'
 end # Loaf
