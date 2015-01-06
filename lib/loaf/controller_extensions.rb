ad0602b44f00fa0bf8530505bf7dce24ca4e9ed7
Piotr Murach <pmurach@gmail.com>
2015-01-07 00:15:30 +0100

    Clean up controller ext.

--- lib/loaf/controller_extensions.rb
+++ lib/loaf/controller_extensions.rb
@@ -2,7 +2,9 @@
 
 module Loaf
   module ControllerExtensions
-
+    # Module injection
+    #
+    # @api private
     def self.included(base)
       base.extend ClassMethods
       base.send :include, InstanceMethods
@@ -10,14 +12,17 @@ module Loaf
     end
 
     module ClassMethods
-
+      # @param [String]
+      #
+      # @api public
       def breadcrumb(name, url, options = {})
         before_filter(options) do |instance|
+          puts 'YUP'
           # instance.send(:add_breadcrumb, _normalize_name(name), url)
-          instance.send(:add_breadcrumb, name, url, options)
+          instance.send(:breadcrumb, name, url, options)
         end
       end
-      alias :add_breadcrumb :breadcrumb
+      alias_method :add_breadcrumb, :breadcrumb
 
       private
 
@@ -32,16 +37,14 @@ module Loaf
           name
         end
       end
-
     end # ClassMethods
 
     module InstanceMethods
-
       # Add collection of nested breadcrumbs.
       # * <tt>collection</tt> - required collection of object for iteration
       # * <tt>field</tt> - required object attribute name
       #
-      def add_breadcrumbs(collection, field, options={})
+      def add_breadcrumbs(collection, field, options = {})
         namespace = nil
         item_set = if _check_if_nested collection
            items = collection.pop
@@ -55,15 +58,28 @@ module Loaf
         end
       end
 
-      def breadcrumb(name, url, options={})
+      # Add breadcrumb
+      #
+      # @param [String] name
+      #
+      # @param [Object] url
+      #
+      # @api public
+      def breadcrumb(name, url, options = {})
         _breadcrumbs << Loaf::Crumb.new(name, url, options[:force])
       end
-      alias :add_breadcrumb :breadcrumb
+      alias_method :add_breadcrumb, :breadcrumb
 
+      # Collection of breadcrumbs
+      #
+      # @api private
       def _breadcrumbs
         @_breadcrumbs ||= []
       end
 
+      # Remove all current breadcrumbs
+      #
+      # @api public
       def clear_breadcrumbs
         _breadcrumbs.clear
       end
@@ -73,8 +89,6 @@ module Loaf
       def _check_if_nested(collection)
         collection.last.is_a? Array
       end
-
     end # InstanceMethods
-
   end # ControllerExtensions
 end # Loaf
