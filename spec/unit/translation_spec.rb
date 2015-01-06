a0eb8f360046c4cf615b908747e96170035f73d9
Piotr Murach <pmurach@gmail.com>
2015-01-06 22:41:54 +0100

    Change translation spec.

--- spec/unit/translation_spec.rb
+++ spec/unit/translation_spec.rb
@@ -1,23 +1,22 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
-describe Loaf::Translation do
+RSpec.describe Loaf::Translation do
 
-  before do
-    I18n.backend = I18n::Backend::Simple.new
-  end
+  before { I18n.backend = I18n::Backend::Simple.new }
 
   it 'translates breadcrumb title' do
     I18n.backend.store_translations 'en', :breadcrumbs => { :home => 'Home'}
-    described_class.breadcrumb_title('breadcrumbs.home').should eql 'Home'
+    expect(described_class.breadcrumb_title('breadcrumbs.home')).to eql('Home')
   end
 
   it 'translates breadcrumb name with default scope' do
-    I18n.backend.store_translations 'en', :breadcrumbs => { :home => 'Home'}
-    described_class.breadcrumb_title('home').should eql 'Home'
+    I18n.backend.store_translations 'en', breadcrumbs: {home: 'Home'}
+    expect(described_class.breadcrumb_title('home')).to eql('Home')
   end
 
   it 'translates breadcrumb name using default option' do
-    described_class.breadcrumb_title('home', :default => 'breadcrumb default name').should eql 'breadcrumb default name'
+    expect(described_class.breadcrumb_title('home', default: 'breadcrumb default name')).to eql('breadcrumb default name')
   end
-
-end # Loaf::Translation
+end
