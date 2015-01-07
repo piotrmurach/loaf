62a228243bf7a2aa48c09b3d5ca5e142cebff1f1
Piotr Murach <pmurach@gmail.com>
2015-01-07 22:16:44 +0100

    Change to simplify and document.

--- lib/loaf/options_validator.rb
+++ lib/loaf/options_validator.rb
@@ -3,12 +3,20 @@
 require 'loaf/errors'
 
 module Loaf
+  # A mixin to validate configuration options
   module OptionsValidator
+    # Check if options are valid or not
+    #
+    # @param [Hash] options
+    #
+    # @return [Boolean]
+    #
+    # @api public
     def valid?(options)
       valid_options = Loaf::Configuration::VALID_ATTRIBUTES
       options.each_key do |key|
-        if !valid_options.include?(key)
-          raise Loaf::InvalidOptions.new(key, valid_options)
+        unless valid_options.include?(key)
+          fail Loaf::InvalidOptions.new(key, valid_options)
         end
       end
       true
