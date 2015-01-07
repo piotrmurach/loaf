7a77ea33bffe549210afc738d0a2d98bdd6f1503
Piotr Murach <pmurach@gmail.com>
2015-01-07 21:35:22 +0100

    Update rails app.

--- spec/rails_app/config/environments/development.rb
+++ spec/rails_app/config/environments/development.rb
@@ -27,4 +27,6 @@ RailsApp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.eager_load = false
 end
--- spec/rails_app/config/environments/test.rb
+++ spec/rails_app/config/environments/test.rb
@@ -11,9 +11,6 @@ RailsApp::Application.configure do
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
 
-  # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -39,4 +36,6 @@ RailsApp::Application.configure do
 
   # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
   config.assets.allow_debugging = true
+
+  config.eager_load = false
 end
