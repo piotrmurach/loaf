92aee043523a9cc721c6ac5bf6347ea7fb760406
Piotr Murach <pmurach@gmail.com>
2015-01-07 22:34:40 +0100

    Change crumb to poro.

--- lib/loaf/crumb.rb
+++ lib/loaf/crumb.rb
@@ -2,5 +2,17 @@
 
 module Loaf
   # Basic crumb container
-  Crumb = Struct.new(:name, :url, :force)
+  class Crumb
+    attr_reader :name
+
+    attr_reader :url
+
+    attr_reader :force
+
+    def initialize(name, url, options = {})
+      @name  = name
+      @url   = url
+      @force = options.fetch(:force) { false }
+    end
+  end
 end # Loaf
