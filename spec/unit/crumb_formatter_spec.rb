b1831d02e853a2b9ea28f8010fa4f39b511580bb
Piotr Murach <pmurach@gmail.com>
2015-01-07 22:02:10 +0100

    Change crumb formatting specs.

--- spec/unit/crumb_formatter_spec.rb
+++ spec/unit/crumb_formatter_spec.rb
@@ -1,33 +1,38 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
-describe Loaf::CrumbFormatter do
+RSpec.describe Loaf::CrumbFormatter, '.format_name' do
+  let(:formatter) {
+    Class.new do
+      extend Loaf::CrumbFormatter
 
-  let(:formatter) { stub.extend(described_class) }
-  let(:crumb) { Loaf::Crumb.new('some randome name', stub) }
-
-  context '#format_name' do
-    it 'does not capitalize by default' do
-      formatted = formatter.format_name(crumb)
-      formatted.should eql 'some randome name'
+      def self.truncate(name, options)
+        name
+      end
     end
+  }
 
-    it 'capitalizes crumb name' do
-      formatted = formatter.format_name(crumb, :capitalize => true)
-      formatted.should eql 'Some randome name'
-    end
-
-    it 'shortens crumb to provided length' do
-      name = 'very long name that is more that 30 characters long'
-      crumb = Loaf::Crumb.new(name, stub)
-      formatter.should_receive(:truncate).with(name, :length => 30).
-        and_return name[0..30]
-      formatter.format_name(crumb, :crumb_length => 30).should eql name[0..30]
-    end
-
-    it 'returns name error if breadcrumb name is nil' do
-      crumb = Loaf::Crumb.new('', stub)
-      formatter.format_name(crumb).should eql '[name-error]'
-    end
+  it 'returns name error if breadcrumb name is nil' do
+    expect(formatter.format_name('')).to eql('')
   end
 
-end # Loaf::CrumbFormatter
+  it "doesn't capitalize by default" do
+    name = 'some random name'
+    formatted = formatter.format_name(name)
+    expect(formatted).to eql(name)
+  end
+
+  it 'capitalizes crumb name with capitalize option' do
+    name = 'some random name'
+    formatted = formatter.format_name(name, capitalize: true)
+    expect(formatted).to eql('Some random name')
+  end
+
+  it 'shortens crumb to provided length' do
+    name = 'very long name that is more that 30 characters long'
+    allow(formatter).to receive(:truncate).with(name, length: 30).
+      and_return(name[0..30])
+    expect(formatter.format_name(name, crumb_length: 30)).to eql(name[0..30])
+  end
+end
