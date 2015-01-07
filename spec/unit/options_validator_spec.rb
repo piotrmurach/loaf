d5a17615ce449a21e7cbac1f61d72c6556640962
Piotr Murach <pmurach@gmail.com>
2015-01-07 22:17:04 +0100

    Change validatior specs.

--- spec/unit/options_validator_spec.rb
+++ spec/unit/options_validator_spec.rb
@@ -1,29 +1,17 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
-describe Loaf::OptionsValidator do
-
+RSpec.describe Loaf::OptionsValidator, '.valid?' do
   let(:klass) { Class.extend Loaf::OptionsValidator }
 
-  describe '.valid?' do
-    context 'when the passed options are valid' do
-      let(:options) do
-        { :crumb_length => 10 }
-      end
-
-      it 'returns true' do
-        klass.valid?(options).should be_true
-      end
-    end
-    context 'when the passed optiosn are invalid' do
-       let(:options) do
-          { :invalid_param => true }
-       end
-
-       it 'raises an error' do
-         expect do
-           klass.valid? options
-         end.to raise_error(Loaf::InvalidOptions)
-       end
-    end
+  it 'validates succesfully known option' do
+    expect(klass.valid?(crumb_length: 10)).to eq(true)
   end
-end # Loaf::OptionsValidator
+
+  it 'validates unknown option with an error' do
+    expect {
+      klass.valid?(invalid_param: true)
+    }.to raise_error(Loaf::InvalidOptions)
+  end
+end
