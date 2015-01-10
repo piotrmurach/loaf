9f09573eabe7bfa28ad4ce07b00eeb8b99c6cf8e
Piotr Murach <pmurach@gmail.com>
2015-01-10 15:05:25 +0100

    Change app to test different scenarios.

--- spec/rails_app/app/controllers/posts_controller.rb
+++ spec/rails_app/app/controllers/posts_controller.rb
@@ -1,16 +1,23 @@
+class Post < Struct.new(:id); end
+
 class PostsController < ApplicationController
-  add_breadcrumb 'Home', 'root_path'
-  add_breadcrumb 'All Posts', :posts_path
+
+  breadcrumb 'Home', :root_path, only: :index
 
   def index
+    breadcrumb 'All Posts', posts_path
+  end
+
+  def show
+    @post = ::Post.new(1)
   end
 
   def new
-    add_breadcrumb 'New Post', 'new_post_path'
+    breadcrumb 'New Post', new_post_path
   end
 
   def create
-    add_breadcrumb 'New Post', 'new_post_path', :force => true
-    render :action => :new
+    breadcrumb 'New Post', new_post_path, force: true
+    render action: :new
   end
 end
