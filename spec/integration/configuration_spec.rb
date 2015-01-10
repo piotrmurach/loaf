0baa505c6d0ef9fce22a8870ff3949a41c0c54f3
Piotr Murach <pmurach@gmail.com>
2015-01-10 15:05:59 +0100

    Fix spec.

--- spec/integration/configuration_spec.rb
+++ spec/integration/configuration_spec.rb
@@ -1,13 +1,12 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
-describe 'setting configuration options' do
-
-  context 'setting default css style for current crumb' do
-    it "contains 'selected' inside the breadcrumb markup" do
-      visit posts_path
-      within '#breadcrumbs' do
-        page.should have_selector('.selected')
-      end
+RSpec.describe 'setting configuration options' do
+  it "contains 'selected' inside the breadcrumb markup" do
+    visit posts_path
+    within '#breadcrumbs' do
+      expect(page).to have_selector('.selected')
     end
   end
 end
