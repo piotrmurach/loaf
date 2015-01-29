22eb3cacc9afbccf7dc80381190573c63c959aa4
Piotr Murach <pmurach@gmail.com>
2015-01-29 21:22:16 +0000

    Remove deprication.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,9 +2,7 @@ require 'capybara/rails'
 require 'capybara/dsl'
 
 RSpec.configure do |c|
-  c.include Capybara::DSL, :example_group => {
-    :file_path => /\bspec\/integration\//
-  }
+  c.include Capybara::DSL, :file_path => /\bspec\/integration\//
 end
 Capybara.default_driver   = :rack_test
 Capybara.default_selector = :css
