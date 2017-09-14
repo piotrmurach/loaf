317d627b5537d749936c308f4818333825404249
Piotr Murach <pmurach@gmail.com>
2017-09-14 19:14:10 +0200

    Change integration tests

--- spec/integration/breadcrumbs_spec.rb
+++ spec/integration/breadcrumbs_spec.rb
@@ -5,21 +5,24 @@ RSpec.describe "breadcrumbs trail" do
 
   it "shows root breadcrumb" do
     visit root_path
+
     within '#breadcrumbs' do
-      expect(page).to have_content('Home')
+      expect(page.html).to include('<a class="selected" href="/">Home</a>')
     end
   end
 
   it "inherits controller breadcrumb and adds index action breadcrumb" do
     visit posts_path
+
     within '#breadcrumbs' do
-      expect(page.html).to include('<a href="/">Home</a>')
-      expect(page.html).to include('<a href="/posts">All Posts</a>')
+      expect(page.html).to include('<a class="" href="/">Home</a>')
+      expect(page.html).to include('<a class="selected" href="/posts">All Posts</a>')
     end
   end
 
   it 'filters out controller breadcrumb and adds new action breadcrumb' do
     visit new_post_path
+
     within '#breadcrumbs' do
       expect(page).to_not have_content('Home')
       expect(page).to have_content('New Post')
@@ -28,8 +31,9 @@ RSpec.describe "breadcrumbs trail" do
 
   it "adds breadcrumb in view with path variable" do
     visit post_path(1)
+
     within '#breadcrumbs' do
-      expect(page.html).to include('<a href="/posts/1">Show Post in view</a>')
+      expect(page.html).to include('<a class="selected" href="/posts/1">Show Post in view</a>')
     end
   end
 
@@ -39,15 +43,14 @@ RSpec.describe "breadcrumbs trail" do
 
     expect(page.current_path).to eq(posts_path)
     within '#breadcrumbs' do
-      expect(page).to have_content('New Post')
-      expect(page).to have_selector('.selected')
+      expect(page.html).to include('<a class="selected" href="/posts/new">New Post</a>')
     end
   end
 
   it "allows for procs in name and url" do
     visit post_comments_path(1)
     within '#breadcrumbs' do
-      expect(page.html).to include('<a href="/posts/1/comments">Post comments</a>')
+      expect(page.html).to include('<a class="selected" href="/posts/1/comments">Post comments</a>')
     end
   end
 end
