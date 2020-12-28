15bbbcfc249d3d34f7420d90f501ba0f09d4e46e
Piotr Murach <pmurach@gmail.com>
2020-12-28 21:05:24 +0100

    Add page scope to resolve conflict between RSpec and Capybara

--- spec/integration/breadcrumb_trail_spec.rb
+++ spec/integration/breadcrumb_trail_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "breadcrumbs trail" do
   it "shows root breadcrumb" do
     visit root_path
 
-    within '#breadcrumbs .selected' do
+    page.within '#breadcrumbs .selected' do
       expect(page.html).to include('<a href="/">Home</a>')
     end
   end
@@ -16,18 +16,18 @@ RSpec.describe "breadcrumbs trail" do
   it "inherits controller breadcrumb and adds index action breadcrumb" do
     visit posts_path
 
-    within '#breadcrumbs' do
+    page.within '#breadcrumbs' do
       expect(page.html).to include('<a href="/">Home</a>')
-      within '.selected' do
-        expect(page.html).to include('<a href="/posts">All Posts</a>')
-      end
+    end
+    page.within '#breadcrumbs .selected' do
+      expect(page.html).to include('<a href="/posts">All Posts</a>')
     end
   end
 
   it 'filters out controller breadcrumb and adds new action breadcrumb' do
     visit new_post_path
 
-    within '#breadcrumbs' do
+    page.within '#breadcrumbs' do
       expect(page).to_not have_content('Home')
       expect(page).to have_content('New Post')
     end
@@ -36,7 +36,7 @@ RSpec.describe "breadcrumbs trail" do
   it "adds breadcrumb in view with path variable" do
     visit post_path(1)
 
-    within '#breadcrumbs .selected' do
+    page.within '#breadcrumbs .selected' do
       expect(page.html).to include('<a href="/posts/1">Show Post in view</a>')
     end
   end
@@ -45,7 +45,7 @@ RSpec.describe "breadcrumbs trail" do
     visit new_post_path
 
     expect(page.current_path).to eq(new_post_path)
-    within '#breadcrumbs' do
+    page.within '#breadcrumbs' do
       expect(page).to have_selector('li.selected', count: 2)
       expect(page.html).to include('<a href="/posts">All</a>')
       expect(page.html).to include('<a href="/posts/new">New Post</a>')
@@ -55,7 +55,7 @@ RSpec.describe "breadcrumbs trail" do
   it "allows for procs in name and url" do
     visit post_comments_path(1)
 
-    within '#breadcrumbs .selected' do
+    page.within '#breadcrumbs .selected' do
       expect(page.html).to include('<a href="/posts/1/comments">Post comments</a>')
     end
   end
@@ -63,7 +63,7 @@ RSpec.describe "breadcrumbs trail" do
   it "allows for procs in name and url without supplying the controller" do
     visit post_comments_path(1)
 
-    within "#breadcrumbs .selected" do
+    page.within "#breadcrumbs .selected" do
       expect(page.html).to include(
         '<a href="/posts/1/comments?no_controller=true">'\
         "Post comments No Controller</a>"
--- spec/integration/configuration_spec.rb
+++ spec/integration/configuration_spec.rb
@@ -1,11 +1,9 @@
 # encoding: utf-8
 
-require 'spec_helper'
-
 RSpec.describe 'setting configuration options' do
   it "contains 'selected' inside the breadcrumb markup" do
     visit posts_path
-    within '#breadcrumbs' do
+    page.within '#breadcrumbs' do
       expect(page).to have_selector('.selected')
     end
   end
